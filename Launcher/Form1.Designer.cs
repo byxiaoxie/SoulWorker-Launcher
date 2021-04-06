9131a41fc6495a5c87852e07ba1cb46afa36a57f
ByXiaoXie <xiaoxiewg@gmail.com>
2021-04-07 06:37:26 +0800

    Launcher

---
+++ Launcher/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Launcher.Properties.Resources.btnClose_dn;
+            this.pictureBox1.Location = new System.Drawing.Point(822, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(22, 22);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::Launcher.Properties.Resources.btnMin_dn;
+            this.pictureBox2.Location = new System.Drawing.Point(794, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(22, 22);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.MouseEnter += new System.EventHandler(this.pictureBox2_MouseEnter);
+            this.pictureBox2.MouseLeave += new System.EventHandler(this.pictureBox2_MouseLeave);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::Launcher.Properties.Resources.btnStart_dn;
+            this.pictureBox3.Location = new System.Drawing.Point(702, 448);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(142, 47);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.pictureBox3.MouseEnter += new System.EventHandler(this.pictureBox3_MouseEnter);
+            this.pictureBox3.MouseLeave += new System.EventHandler(this.pictureBox3_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(856, 507);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainPosition = new System.Drawing.Point(14, 69);
+            this.Name = "Form1";
+            this.ShowInTaskbar = true;
+            this.SkinBack = global::Launcher.Properties.Resources.MainUI;
+            this.SkinShowInTaskbar = false;
+            this.SkinSize = new System.Drawing.Size(915, 630);
+            this.SkinTrankColor = System.Drawing.SystemColors.Control;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Soul Worker Launcher";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Launcher/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using CCWin;
+using Microsoft.Win32;
+
+namespace Launcher
+{
+    public partial class Form1 : SkinMain
+    {
+        bool start = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            this.pictureBox1.Image = global::Launcher.Properties.Resources.btnClose_up;
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            this.pictureBox1.Image = global::Launcher.Properties.Resources.btnClose_dn;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox2_MouseEnter(object sender, EventArgs e)
+        {
+            this.pictureBox2.Image = global::Launcher.Properties.Resources.btnMin_up;
+        }
+
+        private void pictureBox2_MouseLeave(object sender, EventArgs e)
+        {
+            this.pictureBox2.Image = global::Launcher.Properties.Resources.btnMin_dn;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            string fileName = "";
+            start = true;
+            this.pictureBox3.Image = global::Launcher.Properties.Resources.btnStart_disable;
+
+            string RegValue = GetRegistryValue("SoulWorker\\DefaultIcon", "");
+            if (RegValue != "")
+            {
+                RegValue = RegValue.Replace("Launcher.exe", "");
+                fileName = RegValue + "SoulWorker100.exe";
+            }
+            else {
+                fileName = "SoulWorker100.exe";
+            }
+            
+            if (File.Exists(fileName))
+            {
+                string ServerIP = getIP("sw.byxiaoxie.com");
+                //RunCmd(fileName, "IP:" + ServerIP + " PORT:10000 MultiExecuteClient:yes SkipWMModule:yes");
+                RunCmd_Test(fileName, ServerIP);
+                Application.Exit();
+            }
+            else {
+                MessageBox.Show("没有找到游戏客户端.", "Launcher");
+                start = false;
+                this.pictureBox3.Image = global::Launcher.Properties.Resources.btnStart_dn;
+            }
+        }
+
+        private void pictureBox3_MouseEnter(object sender, EventArgs e)
+        {
+            if(!start)
+                this.pictureBox3.Image = global::Launcher.Properties.Resources.btnStart_up;
+        }
+
+        private void pictureBox3_MouseLeave(object sender, EventArgs e)
+        {
+            if (!start)
+                this.pictureBox3.Image = global::Launcher.Properties.Resources.btnStart_dn;
+        }
+
+        /// <summary>
+        /// 域名转IP
+        /// </summary>
+        /// <param name="domain">域名</param>
+        public static string getIP(string domain)
+        {
+            domain = domain.Replace("http://", "").Replace("https://", "");
+            IPHostEntry host = Dns.GetHostEntry(domain);
+            IPEndPoint ip = new IPEndPoint(host.AddressList[0], 0);
+            return ip.Address.ToString();
+        }
+
+        /// <summary>
+        /// 运行cmd命令
+        /// 不显示命令窗口
+        /// </summary>
+        /// <param name="cmdExe">指定应用程序的完整路径</param>
+        /// <param name="cmdStr">执行命令行参数</param>
+        static bool RunCmd(string cmdExe, string cmdStr)
+        {
+            bool result = false;
+            try
+            {
+                using (Process myPro = new Process())
+                {
+                    myPro.StartInfo.FileName = "cmd.exe";
+                    myPro.StartInfo.UseShellExecute = false;
+                    myPro.StartInfo.RedirectStandardInput = true;
+                    myPro.StartInfo.RedirectStandardOutput = true;
+                    myPro.StartInfo.RedirectStandardError = true;
+                    myPro.StartInfo.CreateNoWindow = true;
+                    myPro.Start();
+                    string str = string.Format(@"""{0}"" {1} {2}", cmdExe, cmdStr, "&exit");
+
+                    myPro.StandardInput.WriteLine(str);
+                    myPro.StandardInput.AutoFlush = true;
+                    myPro.WaitForExit();
+
+                    result = true;
+                }
+            }
+            catch
+            {
+
+            }
+            return result;
+        }
+
+        static bool RunCmd_Test(string fileName, string ServerIP)
+        {
+            bool result = false;
+            try
+            {
+                //创建一个进程
+                Process p = new Process();
+                p.StartInfo.FileName = "cmd.exe";
+                p.StartInfo.UseShellExecute = false;//是否使用操作系统shell启动
+                p.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+                p.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
+                p.StartInfo.RedirectStandardError = true;//重定向标准错误输出
+                p.StartInfo.CreateNoWindow = true;//不显示程序窗口
+                p.Start();//启动程序
+
+                p.StandardInput.WriteLine("cd /d "+ fileName.Replace("SoulWorker100.exe", ""));
+                string strCMD = "start " + fileName + " IP:" + ServerIP + " PORT:10000 MultiExecuteClient:yes SkipWMModule:yes";
+                //向cmd窗口发送输入信息
+                p.StandardInput.WriteLine(strCMD + "&exit");
+
+                p.StandardInput.AutoFlush = true;
+
+                //获取cmd窗口的输出信息
+                //string output = p.StandardOutput.ReadToEnd();
+                //等待程序执行完退出进程
+                p.WaitForExit();
+                p.Close();
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message + "\r\n跟踪;" + ex.StackTrace);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 读取注册表值
+        /// </summary>
+        /// <param name="path">指定注册表的完整路径</param>
+        /// <param name="paramName">注册表名称</param>
+        protected string GetRegistryValue(string path, string paramName)
+        {
+            string value = string.Empty;
+            RegistryKey root = Registry.ClassesRoot;
+            RegistryKey rk = root.OpenSubKey(path);
+            if (rk != null)
+            {
+                value = (string)rk.GetValue(paramName, null);
+            }
+            return value;
+        }
+    }
+}
